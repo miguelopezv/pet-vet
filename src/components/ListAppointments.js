63632113fd86a0e7c1992600d6d58c3c5058fc85
Miguel López <miguellopez@vitalware.com>
2019-07-29 19:24:53 -0500

    component to list all appointments

---
+++ src/components/ListAppointments.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import Appointment from './Appointment';
+
+const ListAppointments = ({ appointments, removeAppointment }) => {
+  const message = Object.keys(appointments).length
+    ? 'No Appointments'
+    : 'Appointments Manager';
+
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title-text-center">{message}</h2>
+        <div className="lista-citas">
+          {appointments.map(appointment => (
+            <Appointment
+              key={appointment.id}
+              appointment={appointment}
+              removeAppointment={removeAppointment}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ListAppointments.propTypes = {
+  appointments: PropTypes.array.isRequired,
+  removeAppointment: PropTypes.func.isRequired
+};
+
+export default ListAppointments;
