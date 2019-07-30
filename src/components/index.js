e2abd52e219a4fd50e0f433c4a78988abafc7e2f
Miguel López <miguellopez@vitalware.com>
2019-07-29 19:25:29 -0500

    barrel

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Header from './Header';
+import NewAppointment from './NewAppointment';
+import ListAppointments from './ListAppointments';
+
+export { Header, NewAppointment, ListAppointments };
